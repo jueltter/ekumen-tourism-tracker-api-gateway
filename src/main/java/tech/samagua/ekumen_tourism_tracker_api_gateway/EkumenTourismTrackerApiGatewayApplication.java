c4ba39249af598290c5a15b661161c52d5de25d8
Stalin Amagua <jueltter@users.noreply.github.com>
2026-01-17 15:37:25 -0500

    initial commit

---
+++ src/main/java/tech/samagua/ekumen_tourism_tracker_api_gateway/EkumenTourismTrackerApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package tech.samagua.ekumen_tourism_tracker_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkumenTourismTrackerApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkumenTourismTrackerApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/samagua/ekumen_tourism_tracker_api_gateway/EkumenTourismTrackerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.samagua.ekumen_tourism_tracker_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkumenTourismTrackerApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
